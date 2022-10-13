a195d933b6e26eb6d1b1b34a84acd41d9e428876
Clara0919 <mansyuan.he@gmail.com>
2022-10-13 11:40:23 +0800

    日曆初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template lang="">
+  <div>
+    <calendar-part></calendar-part>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<script>
+import calendarPart from "../src/components/calendarPart.vue";
+export default {
+  components: { calendarPart },
+};
+</script>
+<style lang="">
 </style>
---
+++ src/components/calendarPart.vue
@@ -0,0 +1,14 @@
+<template lang="">
+   <v-calendar />
+  <!-- <v-date-picker v-model="date" /> -->
+</template>
+<script>
+import "v-calendar/dist/style.css";
+export default {
+  data() {
+    return { date: [] };
+  },
+};
+</script>
+<style lang="">
+</style>
